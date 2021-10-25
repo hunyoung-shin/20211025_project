f6997375f47871f0a1ece6c613177ef1875741ca
etrees <etrees@DESKTOP-I60DEVU>
2021-10-25 15:57:21 +0900

    22222

--- src/main/java/com/spring/special/SpecialVo.java
+++ src/main/java/com/spring/special/SpecialVo.java
@@ -2,5 +2,54 @@ package com.spring.special;
 
 public class SpecialVo {
 
-	
+			String speNum;	
+			String speTitle;	
+			String speImg;	
+			String speTheme;	
+			String speNoti;
+			String linkImg;	
+			String brandId;
+			public String getSpeNum() {
+				return speNum;
+			}
+			public void setSpeNum(String speNum) {
+				this.speNum = speNum;
+			}
+			public String getSpeTitle() {
+				return speTitle;
+			}
+			public void setSpeTitle(String speTitle) {
+				this.speTitle = speTitle;
+			}
+			public String getSpeImg() {
+				return speImg;
+			}
+			public void setSpeImg(String speImg) {
+				this.speImg = speImg;
+			}
+			public String getSpeTheme() {
+				return speTheme;
+			}
+			public void setSpeTheme(String speTheme) {
+				this.speTheme = speTheme;
+			}
+			public String getSpeNoti() {
+				return speNoti;
+			}
+			public void setSpeNoti(String speNoti) {
+				this.speNoti = speNoti;
+			}
+			public String getLinkImg() {
+				return linkImg;
+			}
+			public void setLinkImg(String linkImg) {
+				this.linkImg = linkImg;
+			}
+			public String getBrandId() {
+				return brandId;
+			}
+			public void setBrandId(String brandId) {
+				this.brandId = brandId;
+			}	
+			
 }
